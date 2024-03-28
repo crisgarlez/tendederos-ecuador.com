43ac8a6beef2fd961f58007f4bd14b0e55fa23a4
crisgarlez <escipion25@gmail.com>
2024-03-28 17:15:46 -0500

    mobile menu fix

--- components/Header.tsx
+++ components/Header.tsx
@@ -86,14 +86,6 @@ const Header = () => {
         </div>
         <div className="hidden lg:flex lg:gap-x-12">
           {navigation.map((item) => (
-            // <a
-            //   key={item.name}
-            //   href={item.href}
-            //   className="text-sm font-semibold leading-6 text-gray-900"
-            // >
-            //   {item.name}
-            // </a>
-
             <Link
               key={item.name}
               href={item.href}
@@ -130,18 +122,11 @@ const Header = () => {
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="space-y-2 py-6">
                 {navigation.map((item) => (
-                  // <a
-                  //   key={item.name}
-                  //   href={item.href}
-                  //   className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                  // >
-                  //   {item.name}
-                  // </a>
-
                   <Link
                     key={item.name}
                     href={item.href}
                     className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                    onClick={() => setMobileMenuOpen(false)}
                   >
                     {item.name}
                   </Link>
